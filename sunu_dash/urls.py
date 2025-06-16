6832928c1f63da47443aa759b41478976ade9f8f
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2025-06-16 06:13:26 -0400

    configuration on file_upload app to the project

--- sunu_dash/urls.py
+++ sunu_dash/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("users.urls")),
+    path("files/", include("file_upload.urls")),
 ]
