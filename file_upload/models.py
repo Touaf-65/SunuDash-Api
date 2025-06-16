677adccf87b6e85791240e73d2bee4fb03aeb22f
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2025-06-16 09:32:28 -0400

    file model

--- file_upload/models.py
+++ file_upload/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import CustomUser as User
 
-# Create your models here.
+class File(models.Model):
+    FILE_TYPE_CHOICES = [
+        ('stat', 'Fichier Statistique'),
+        ('recap', 'Fichier Récap'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='uploads/')
+    file_type = models.CharField(max_length=5, choices=FILE_TYPE_CHOICES)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    size = models.PositiveIntegerField()
+
+    def save(self, *args, **kwargs):
+        self.size = self.file.size
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.file.name
