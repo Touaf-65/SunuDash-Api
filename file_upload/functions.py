a7957604c1701f5837bfbd186d9a5759fb069ed8
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2025-06-18 02:17:03 -0400

    write some useful functions for analysis

--- file_upload/functions.py
+++ file_upload/functions.py
@@ -10,3 +10,20 @@ def open_excel_csv(file):
     else:
         return Response({"error": "Le fichier récap doit être au format Excel (xlsx/xls) ou CSV."},)
     return df
+
+def replace_invalid_numeric_values(rec,column):
+    rec[column] = pd.to_numeric(rec[column], errors='coerce').fillna(0)
+
+
+def convert_dates_datetime(rec, column):
+    column_type = rec[column].dtype
+
+    if column_type == 'object':
+        rec[column] = pd.to_datetime(rec[column], errors='coerce')
+    elif column_type in ['int64', 'int32']:
+        rec[column] = pd.to_datetime(rec[column], origin='1899-12-30', unit='D')
+    else:
+        pass
+    return rec
+
+    
